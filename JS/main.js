1b72513033b92402a7106d9d61d3cf7e2f53c648
Emilie <emilielayani@gmail.com>
2018-11-07 10:15:31 +0200

    Creation of the folder : html + css + js

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+var drawingZone = document.querySelector(".drawingZone");
+
+function draw(event) {
+    console.log("in the draw function")
+    var pencil = document.createElement("div");
+    var drawingZone = document.querySelector(".drawingZone");
+    drawingZone.appendChild(pencil);
+
+    // pencil style
+    pencil.style.background = "black";
+    pencil.style.position = "absolute";
+    pencil.style.height = "5px";
+    pencil.style.width = "5px";
+
+    // pencil position
+    var x = event.clientX;
+    var y = event.clientY;
+    console.log(`${x+'px x |'},${y+'px y'}`);
+    pencil.style.position = "absolute";
+    pencil.style.left = x + "px";
+    pencil.style.top = y + "px";
+}
+function move(event) {
+    drawingZone.addEventListener("mousemove", draw);
+}
+function stop(event) {
+    console.log('Im up')
+    //
+}
+$('.drawingZone').on('mouseup', function(e) {
+    e.preventDefault();
+    $('.drawingZone').off('mousemove.draw, mousedown.move');
+});
+
+drawingZone.addEventListener("mousedown", move);
+drawingZone.addEventListener("mouseup", stop);
+
